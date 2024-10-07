e5eb5757a412264f310431adccb2fbe9efccca92
arryerhan <arryerhan@gmail.com>
2024-10-06 22:27:34 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+//! -------------------- IMPORTS --------------------
+
+import { API_URL,IMG_PATH,SEARCH_API,showMovies,form,search,main } from "./ui.js";
+
+//! -------------------- FETCH DATA --------------------
+getMovies(API_URL)
+
+async function getMovies(url) {
+  const res = await fetch(url)
+  const data = await res.json()
+
+  showMovies(data.results)
+}
+
+//! -------------------- SEARCH AREA --------------------
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const searchTerm = search.value
+
+  if (searchTerm && searchTerm !== '') {
+    getMovies(SEARCH_API + searchTerm)
+
+    search.value = ''
+  } else {
+    window.location.reload()
+  }
+})
+
+
+
