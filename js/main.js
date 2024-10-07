e7db3e8ec5a29f8a81b89573823c50e9f051c852
arryerhan <arryerhan@gmail.com>
2024-10-07 14:20:52 -0400

    general update

--- js/main.js
+++ js/main.js
@@ -2,30 +2,64 @@
 
 import { API_URL,IMG_PATH,SEARCH_API,showMovies,form,search,main } from "./ui.js";
 
+
+
+
+
+
+
+
+
+
 //! -------------------- FETCH DATA --------------------
+
 getMovies(API_URL)
 
+let data;
+
 async function getMovies(url) {
+
   const res = await fetch(url)
-  const data = await res.json()
+
+  data = await res.json()
 
   showMovies(data.results)
 }
 
+window.addEventListener("DOMContentLoaded",() => {
+
+getMovies(API_URL)
+
+.then(()=>showMovies(data.results))
+
+});
+
+
+
+
+
+
+
 //! -------------------- SEARCH AREA --------------------
 
 form.addEventListener('submit', (e) => {
+
   e.preventDefault()
 
   const searchTerm = search.value
 
   if (searchTerm && searchTerm !== '') {
+
     getMovies(SEARCH_API + searchTerm)
 
     search.value = ''
+
   } else {
+
     window.location.reload()
+
   }
+
 })
 
 
--- js/ui.js
+++ js/ui.js
@@ -1,52 +1,100 @@
+
 //! -------------------- API PATHS --------------------
 
 export const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1';
+
 export const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+
 export const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
+
+
+
+
+
+
 
 //! -------------------- HTML ELEMENTS --------------------
 
 export const form = document.getElementById('form')
+
 export const search = document.getElementById('search')
+
 export const main = document.getElementById('main')
+
+
+
+
+
 
 //! -------------------- MOVIE CARDS --------------------
 
+
 export const  showMovies = (data) => {
-    main.innerHTML = ''
+
+  main.innerHTML = ''
   
-    data.forEach((movie) => {
-      const { title, poster_path, vote_average, overview } = movie
+
+  data.forEach((movie) => {
+
+    const { title, poster_path, vote_average, overview } = movie
   
-      const movieEl = document.createElement('div')
-      movieEl.classList.add('movie')
+    const movieEl = document.createElement('div')
+
+    movieEl.classList.add('movie')
       
-      movieEl.innerHTML = `
-      <div class="movie-card">
-          <img src="${IMG_PATH + poster_path}" alt="${title}"/>
-      </div>
-        <div class="movie-info">
-        <h3>${title}</h3>
-        <span class="${getClassByRate(vote_average)}">${vote_average.toFixed(1)}</span>
-        </div>
-        <div class="overview">
-        <h3> ${title}  <small> Overview </small> </h3>
-        <p>${overview}</p>
-        </div>
-      `
+
+    movieEl.innerHTML = 
+    
+    `
+    <div class="movie-card">
+
+    <img src="${IMG_PATH + poster_path}" alt="${title}"/>
+
+    </div>
+
+    <div class="movie-info">
+
+    <h3>${title}</h3>
+
+    <span class="${getClassByRate(vote_average)}">${vote_average.toFixed(1)}</span>
+
+    </div>
+
+    <div class="overview">
+
+    <h3> ${title}  <small> Overview </small> </h3>
+
+    <p>${overview}</p>
+
+    </div>
+
+    `
       main.appendChild(movieEl)
       
     })
   }
 
+
+
+
+
+
   //! -------------------- RATING CLASS --------------------
 
 export const getClassByRate = (vote) => {
-    if (vote >= 7.5) {
-      return 'green'
-    } else if (vote >=5.5 ) {
-      return 'orange'
-    } else {
-      return 'red'
-    }
+   
+  if (vote >= 7.5) {
+  
+    return 'green'
+  
+  } else if (vote >=5.5 ) {
+  
+    return 'orange'
+  
+  } else {
+  
+    return 'red'
+  
   }
+  
+}
